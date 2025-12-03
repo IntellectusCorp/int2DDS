3856f138a1d5f78d3b7f785e393229bab93ab70a
Intellectus-Garam <ran.kim@int2.us>
2025-12-03 17:17:23 +0900

    TEST: Add deadline tests (#5)

--- dds/tests/entity_factory.rs
+++ dds/tests/entity_factory.rs
@@ -4,7 +4,10 @@ use common::*;
 use int2dds::{
     common::instance_handle::InstanceHandle,
     dcps::{
-        domain::{domain_participant_factory::DomainParticipantFactory, qos::DomainParticipantQos},
+        domain::{
+            domain_participant, domain_participant_factory::DomainParticipantFactory,
+            qos::DomainParticipantQos,
+        },
         infrastructure::{qos_policy::EntityFactoryQosPolicy, status::StatusMask},
         publication::qos::{DataWriterQos, PublisherQos},
         topic::qos::TopicQos,
@@ -20,36 +23,16 @@ fn autoenable_created_entities_true() {
         .create_participant(domain_id, DomainParticipantQos::default(), None, StatusMask::default())
         .unwrap();
 
-    let topic = participant
-        .create_topic::<KeyedDataType>(
-            "test_topic",
-            "KeyedData",
-            TopicQos::default(),
-            None,
-            StatusMask::default(),
-        )
-        .unwrap();
-
     // Publisher with autoenable_created_entities = true (default)
-    let publisher =
-        participant.create_publisher(PublisherQos::default(), None, StatusMask::default()).unwrap();
-
-    let writer = publisher
-        .create_datawriter::<KeyedDataType>(
-            &topic,
-            DataWriterQos::default(),
-            None,
-            StatusMask::default(),
-        )
-        .unwrap();
+    let data_writer = create_datawriter(&participant, DataWriterQos::default());
 
     // DataWriter should be automatically enabled
-    let res = writer.write(&KeyedDataType::new(1, 100), InstanceHandle::NIL);
+    let res = data_writer.write(&KeyedDataType::new(1, 100), InstanceHandle::NIL);
     assert!(res.is_ok());
 }
 
 #[test]
-fn autoenable_created_entities_false() {
+fn autoenable_created_entities_false_direct_parent() {
     let domain_id = next_domain_id();
     let factory = DomainParticipantFactory::get_instance();
 
@@ -94,3 +77,29 @@ fn autoenable_created_entities_false() {
     let res_2 = writer.write(&KeyedDataType::new(1, 100), InstanceHandle::NIL);
     assert!(res_2.is_ok());
 }
+
+// #[test]
+// fn autoenable_created_entities_false_dp() {
+//     let domain_id = next_domain_id();
+//     let factory = DomainParticipantFactory::get_instance();
+
+//     let mut domain_participant_qos = DomainParticipantQos::default();
+//     domain_participant_qos.entity_factory = EntityFactoryQosPolicy { autoenable_created_entities: false };
+
+//     let participant = factory
+//         .create_participant(domain_id, DomainParticipantQos::default(), None, StatusMask::default())
+//         .unwrap();
+
+//     let writer = create_datawriter(&participant, DataWriterQos::default());
+
+//     // write() should fail when not enabled
+//     let res = writer.write(&KeyedDataType::new(1, 100), InstanceHandle::NIL);
+//     assert!(res.is_err());
+
+//     // Manually enable the writer
+//     writer.enable().unwrap();
+
+//     // Now write() should succeed
+//     let res_2 = writer.write(&KeyedDataType::new(1, 100), InstanceHandle::NIL);
+//     assert!(res_2.is_ok());
+// }
