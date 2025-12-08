e02c29bea7f7e2e6c744516acf17b5365d6ba104
Intellectus-Hyeonseo <hs.son@int2.us>
2025-12-08 14:46:32 +0900

    FEAT: Handle Data::Enum in DdsType derive macro (#18)

--- derive/src/dds_type.rs
+++ derive/src/dds_type.rs
@@ -2,12 +2,16 @@ use quote::quote;
 use syn::{Data, DeriveInput, Fields};
 
 use crate::codegen::{
-    generate_additional_derives, generate_field_deserialization,
+    generate_additional_derives, generate_enum_cdr_deserialize_impl,
+    generate_enum_cdr_serialize_impl, generate_enum_xcdr_deserialize_impl,
+    generate_enum_xcdr_serialize_impl, generate_field_deserialization,
     generate_field_deserialization_xcdr, generate_field_serialization,
     generate_field_serialization_xcdr, generate_key_field_serialization,
     generate_key_field_serialization_xcdr, generate_key_methods, generate_multi_key_methods,
     generate_non_key_field_serialization, generate_non_key_field_serialization_xcdr,
-    parse_field_attributes, KeyFieldInfo, MultiKeyFieldInfo,
+    generate_union_cdr_deserialize_impl, generate_union_cdr_serialize_impl,
+    generate_union_xcdr_deserialize_impl, generate_union_xcdr_serialize_impl, is_c_style_enum,
+    parse_field_attributes, parse_repr_attribute, KeyFieldInfo, MultiKeyFieldInfo,
 };
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
@@ -26,20 +30,37 @@ struct DdsTypeConfig {
 
 pub fn derive_dds_type_impl(input: &DeriveInput) -> proc_macro2::TokenStream {
     let name = &input.ident;
-    let type_support_name = quote::format_ident!("{}TypeSupport", name);
 
     // Parse dds_type attributes
     let type_config = parse_dds_type_attributes(input);
     let crate_path = &type_config.crate_path;
 
-    // Check if it's a struct
-    let fields = match &input.data {
-        Data::Struct(data) => match &data.fields {
-            Fields::Named(fields) => &fields.named,
-            _ => panic!("DdsType can only be derived for structs with named fields"),
-        },
-        _ => panic!("DdsType can only be derived for structs"),
-    };
+    // Handle struct vs enum
+    match &input.data {
+        Data::Struct(data) => {
+            let fields = match &data.fields {
+                Fields::Named(fields) => &fields.named,
+                _ => panic!("DdsType can only be derived for structs with named fields"),
+            };
+            derive_struct_impl(input, name, fields, &type_config, crate_path)
+        }
+        Data::Enum(data) => {
+            let variants = &data.variants;
+            derive_enum_impl(input, name, variants, &type_config, crate_path)
+        }
+        Data::Union(_) => panic!("DdsType cannot be derived for Rust unions. Use enum instead."),
+    }
+}
+
+/// Generate DdsType implementation for struct types
+fn derive_struct_impl(
+    input: &DeriveInput,
+    name: &syn::Ident,
+    fields: &syn::punctuated::Punctuated<syn::Field, syn::token::Comma>,
+    type_config: &DdsTypeConfig,
+    crate_path: &proc_macro2::TokenStream,
+) -> proc_macro2::TokenStream {
+    let type_support_name = quote::format_ident!("{}TypeSupport", name);
 
     // Find all key fields - all fields with #[dds(key)] attribute
     let all_key_fields = find_all_key_fields(fields);
@@ -142,6 +163,199 @@ pub fn derive_dds_type_impl(input: &DeriveInput) -> proc_macro2::TokenStream {
         #xcdr_serialize_impl
         #xcdr_deserialize_impl
         #additional_derives
+    }
+}
+
+/// Generate DdsType implementation for enum types (DDS enum or union)
+fn derive_enum_impl(
+    input: &DeriveInput,
+    name: &syn::Ident,
+    variants: &syn::punctuated::Punctuated<syn::Variant, syn::token::Comma>,
+    _type_config: &DdsTypeConfig,
+    crate_path: &proc_macro2::TokenStream,
+) -> proc_macro2::TokenStream {
+    let type_support_name = quote::format_ident!("{}TypeSupport", name);
+
+    // Parse #[repr(...)] attribute for discriminant type
+    let disc_type = parse_repr_attribute(&input.attrs);
+
+    // Determine if this is a C-style enum (DDS enum) or has data (DDS union)
+    let is_enum = is_c_style_enum(variants);
+
+    // Generate TypeSupport struct
+    let type_support_struct = quote! {
+        #[derive(Default)]
+        pub struct #type_support_name;
+    };
+
+    // Generate DdsType trait implementation
+    let dds_type_impl = quote! {
+        impl #crate_path::dcps::topic::type_support::DdsType for #name {
+            type TypeSupport = #type_support_name;
+        }
+    };
+
+    // Generate TypeSupport trait implementation for enum/union
+    let type_support_impl = generate_enum_type_support_impl(&type_support_name, name, crate_path);
+
+    // Generate CdrSerialize/CdrDeserialize and XcdrSerialize/XcdrDeserialize
+    let (cdr_serialize_impl, cdr_deserialize_impl, xcdr_serialize_impl, xcdr_deserialize_impl) =
+        if is_enum {
+            // C-style enum (DDS enum)
+            (
+                generate_enum_cdr_serialize_impl(name, variants, crate_path, disc_type),
+                generate_enum_cdr_deserialize_impl(name, variants, crate_path, disc_type),
+                generate_enum_xcdr_serialize_impl(name, variants, crate_path, disc_type),
+                generate_enum_xcdr_deserialize_impl(name, variants, crate_path, disc_type),
+            )
+        } else {
+            // Enum with data (DDS union)
+            (
+                generate_union_cdr_serialize_impl(name, variants, crate_path, disc_type),
+                generate_union_cdr_deserialize_impl(name, variants, crate_path, disc_type),
+                generate_union_xcdr_serialize_impl(name, variants, crate_path, disc_type),
+                generate_union_xcdr_deserialize_impl(name, variants, crate_path, disc_type),
+            )
+        };
+
+    // Generate additional derives (Default, Debug, Clone, PartialEq, speedy traits)
+    let additional_derives = generate_additional_derives(input, name);
+
+    quote! {
+        #type_support_struct
+        #type_support_impl
+        #dds_type_impl
+        #cdr_serialize_impl
+        #cdr_deserialize_impl
+        #xcdr_serialize_impl
+        #xcdr_deserialize_impl
+        #additional_derives
+    }
+}
+
+/// Generate TypeSupport implementation for enum/union types
+fn generate_enum_type_support_impl(
+    type_support_name: &syn::Ident,
+    name: &syn::Ident,
+    crate_path: &proc_macro2::TokenStream,
+) -> proc_macro2::TokenStream {
+    quote! {
+        impl #crate_path::dcps::topic::type_support::TypeSupport for #type_support_name {
+            fn get_type_name(&self) -> &str {
+                stringify!(#name)
+            }
+
+            fn type_id(&self) -> std::any::TypeId {
+                std::any::TypeId::of::<#name>()
+            }
+
+            fn is_compute_key_provided(&self) -> bool {
+                false
+            }
+
+            fn get_extensibility_kind(&self) -> #crate_path::serialize::xcdr::ExtensibilityKind {
+                #crate_path::serialize::xcdr::ExtensibilityKind::Final
+            }
+
+            fn serialize(&self, data: &dyn std::any::Any) -> #crate_path::dcps::core::error::DdsResult<#crate_path::rtps::common::types::SerializedData> {
+                self.serialize_with_format(data, &#crate_path::dcps::topic::type_support::SerializationFormat::Cdr)
+            }
+
+            fn deserialize(&self, data: &[u8]) -> #crate_path::dcps::core::error::DdsResult<Box<dyn std::any::Any>> {
+                self.deserialize_with_format(data, &#crate_path::dcps::topic::type_support::SerializationFormat::Cdr)
+            }
+
+            fn serialize_with_format(&self, data: &dyn std::any::Any, format: &#crate_path::dcps::topic::type_support::SerializationFormat) -> #crate_path::dcps::core::error::DdsResult<#crate_path::rtps::common::types::SerializedData> {
+                if let Some(typed_data) = data.downcast_ref::<#name>() {
+                    match format {
+                        #crate_path::dcps::topic::type_support::SerializationFormat::Cdr => {
+                            use #crate_path::serialize::{cdr::CdrSerializer, BufferManager};
+                            use #crate_path::serialize::cdr::CdrSerialize;
+
+                            let mut serializer = CdrSerializer::with_capacity(true, 64);
+                            serializer.write_encapsulation_header()
+                                .map_err(|e| #crate_path::dcps::core::error::DdsError::Error(e.to_string()))?;
+
+                            typed_data.serialize_cdr(&mut serializer)
+                                .map_err(|e| #crate_path::dcps::core::error::DdsError::Error(e.to_string()))?;
+
+                            let bytes = serializer.into_bytes();
+                            Ok(std::sync::Arc::from(bytes.into_boxed_slice()))
+                        }
+                        #crate_path::dcps::topic::type_support::SerializationFormat::Xcdr { extensibility_kind, .. } => {
+                            use #crate_path::serialize::{xcdr::Xcdr2Serializer, BufferManager};
+                            use #crate_path::serialize::cdr::XcdrSerialize;
+
+                            let mut serializer = Xcdr2Serializer::with_capacity(true, *extensibility_kind, 64);
+                            serializer.write_encapsulation_header()
+                                .map_err(|e| #crate_path::dcps::core::error::DdsError::Error(e.to_string()))?;
+
+                            typed_data.serialize_xcdr(&mut serializer)
+                                .map_err(|e| #crate_path::dcps::core::error::DdsError::Error(e.to_string()))?;
+
+                            let bytes = serializer.into_bytes();
+                            Ok(std::sync::Arc::from(bytes.into_boxed_slice()))
+                        }
+                    }
+                } else {
+                    Err(#crate_path::dcps::core::error::DdsError::BadParameter)
+                }
+            }
+
+            fn deserialize_with_format(&self, data: &[u8], format: &#crate_path::dcps::topic::type_support::SerializationFormat) -> #crate_path::dcps::core::error::DdsResult<Box<dyn std::any::Any>> {
+                match format {
+                    #crate_path::dcps::topic::type_support::SerializationFormat::Cdr => {
+                        use #crate_path::serialize::cdr::CdrDeserializer;
+                        use #crate_path::serialize::cdr::CdrDeserialize;
+
+                        let mut deserializer = CdrDeserializer::new(data)
+                            .map_err(|e| #crate_path::dcps::core::error::DdsError::Error(e.to_string()))?;
+
+                        let result = #name::deserialize_cdr(&mut deserializer)
+                            .map_err(|e| #crate_path::dcps::core::error::DdsError::Error(e.to_string()))?;
+
+                        Ok(Box::new(result))
+                    }
+                    #crate_path::dcps::topic::type_support::SerializationFormat::Xcdr { .. } => {
+                        use #crate_path::serialize::xcdr::Xcdr2Deserializer;
+                        use #crate_path::serialize::cdr::XcdrDeserialize;
+
+                        let mut deserializer = Xcdr2Deserializer::new(data)
+                            .map_err(|e| #crate_path::dcps::core::error::DdsError::Error(e.to_string()))?;
+
+                        let result = #name::deserialize_xcdr(&mut deserializer)
+                            .map_err(|e| #crate_path::dcps::core::error::DdsError::Error(e.to_string()))?;
+
+                        Ok(Box::new(result))
+                    }
+                }
+            }
+
+            fn serialize_key(&self, _data: &dyn std::any::Any) -> #crate_path::dcps::core::error::DdsResult<#crate_path::rtps::common::types::SerializedData> {
+                Err(#crate_path::dcps::core::error::DdsError::Error("Enum/Union types do not support key serialization".to_string()))
+            }
+
+            fn deserialize_key(&self, _serialized_key: &[u8]) -> #crate_path::dcps::core::error::DdsResult<Box<dyn std::any::Any + Send + Sync>> {
+                Err(#crate_path::dcps::core::error::DdsError::Error("Enum/Union types do not support key deserialization".to_string()))
+            }
+
+            fn compute_key(&self, _data: &dyn std::any::Any) -> #crate_path::common::instance_handle::InstanceHandle {
+                #crate_path::common::instance_handle::InstanceHandle::default()
+            }
+
+            fn serialize_key_and_non_key(&self, data: &dyn std::any::Any) -> #crate_path::dcps::core::error::DdsResult<(#crate_path::rtps::common::types::SerializedData, #crate_path::rtps::common::types::SerializedData)> {
+                let full_data = self.serialize(data)?;
+                Ok((full_data.clone(), full_data))
+            }
+
+            fn get_field_value(&self, _data: &dyn std::any::Any, _field_path: &str) -> #crate_path::dcps::core::error::DdsResult<#crate_path::topic::sql::ast::Parameter> {
+                Err(#crate_path::dcps::core::error::DdsError::Error("Enum/Union types do not support field access".to_string()))
+            }
+
+            fn has_field(&self, _field_path: &str) -> bool {
+                false
+            }
+        }
     }
 }
 
