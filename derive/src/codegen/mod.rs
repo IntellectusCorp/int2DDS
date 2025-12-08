93bf4a9314c73ef9ba5d899182cadf715c73a20a
Intellectus-Hyeonseo <hs.son@int2.us>
2025-12-08 14:45:34 +0900

    FEAT: Add enum/union utility functions and DiscriminantType (#18)

--- derive/src/codegen/mod.rs
+++ derive/src/codegen/mod.rs
@@ -1,9 +1,13 @@
 pub mod derives;
+pub mod enum_ops;
 pub mod field_ops;
 pub mod key_methods;
+pub mod union_ops;
 pub mod utils;
 
 pub use derives::*;
+pub use enum_ops::*;
 pub use field_ops::*;
 pub use key_methods::*;
+pub use union_ops::*;
 pub use utils::*;
