347465b10f71b03af9f0f757ef6ecaac3815b894
Intellectus-Shinhaeseul <hs.shin@int2.us>
2025-12-16 16:11:31 +0900

    FEAT: Add QoS profile support to Subscriber (#8) - Add create_datareader_with_profile() for profile-based DataReader creation - Add get_datareader_qos_from_profile() helper method delegating to Factory

--- dds/src/dcps/subscription/subscriber.rs
+++ dds/src/dcps/subscription/subscriber.rs
@@ -28,7 +28,9 @@ use std::{
 use crate::{
     common::instance_handle::InstanceHandle,
     core::error::{DdsError, DdsResult},
-    domain::domain_participant::DomainParticipant,
+    domain::{
+        domain_participant::DomainParticipant, domain_participant_factory::DomainParticipantFactory,
+    },
     infrastructure::{
         domain_entity::DomainEntity,
         entity::{
@@ -290,6 +292,17 @@ impl Subscriber {
         }
 
         Ok(datareader)
+    }
+
+    pub fn create_datareader_with_profile<Foo: DdsType>(
+        &self,
+        topic_description: &dyn TopicDescription,
+        qos_path: &str,
+        listener: Option<Arc<dyn DataReaderListener<Foo = Foo>>>,
+        mask: StatusMask,
+    ) -> DdsResult<DataReader<Foo>> {
+        let qos = self.get_datareader_qos_from_profile(qos_path)?;
+        self.create_datareader::<Foo>(topic_description, qos, listener, mask)
     }
 
     pub fn delete_datareader<Foo: 'static + Clone + Debug>(
@@ -822,6 +835,11 @@ impl Subscriber {
             Ok(default_datareader_qos) => Ok(default_datareader_qos.clone()),
             Err(e) => Err(DdsError::Error(e.to_string())),
         }
+    }
+
+    pub fn get_datareader_qos_from_profile(&self, qos_path: &str) -> DdsResult<DataReaderQos> {
+        self.is_deleted()?;
+        DomainParticipantFactory::get_instance().get_datareader_qos_from_profile(qos_path)
     }
 
     pub fn copy_from_topic_qos(
