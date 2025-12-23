4bd7be751bfb1b14021f2ab2d8abb5cf33bb043b
Intellectus-Garam <ran.kim@int2.us>
2025-12-23 19:01:30 +0900

    CHORE: Remove logs (#78)

--- dds/tests/data_lifecycle.rs
+++ dds/tests/data_lifecycle.rs
@@ -2,11 +2,7 @@ mod common;
 
 use common::*;
 use int2dds::{
-    common::{
-        env::{set_file_log_level, set_log_type},
-        instance_handle::InstanceHandle,
-        log::{LogLevel, LogType},
-    },
+    common::instance_handle::InstanceHandle,
     dcps::{
         core::time::Duration,
         domain::{domain_participant_factory::DomainParticipantFactory, qos::DomainParticipantQos},
@@ -24,9 +20,6 @@ use int2dds::{
 
 #[test]
 fn test_autodispose_unregistered_instances_true() {
-    // set_log_type(LogType::File);
-    // set_file_log_level(LogLevel::Debug);
-
     let domain_id = next_domain_id();
     let factory = DomainParticipantFactory::get_instance();
     let participant = factory
@@ -88,9 +81,6 @@ fn test_autodispose_unregistered_instances_true() {
 
 #[test]
 fn test_autodispose_unregistered_instances_false() {
-    set_log_type(LogType::File);
-    set_file_log_level(LogLevel::Debug);
-
     let domain_id = next_domain_id();
     let factory = DomainParticipantFactory::get_instance();
     let participant = factory
@@ -148,10 +138,6 @@ fn test_autodispose_unregistered_instances_false() {
             &[InstanceStateKind::NOT_ALIVE_NO_WRITERS_INSTANCE_STATE],
         )
         .unwrap();
-
-    for sample in samples.clone() {
-        println!("Sample Info: {:?}", sample.sample_info());
-    }
 
     assert!(samples.len() == 2);
 }
