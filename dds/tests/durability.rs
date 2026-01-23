5fdbaccb32643717628ec9bfc256e46f85685814
Intellectus-Garam <ran.kim@int2.us>
2026-01-23 17:15:58 +0900

    TEST: Add test for reliable volatile (#109)

--- dds/tests/durability.rs
+++ dds/tests/durability.rs
@@ -22,7 +22,7 @@ use int2dds::{
 };
 
 #[test]
-fn test_volatile() {
+fn test_volatile_best_effort() {
     let domain_id = next_domain_id();
     let factory = DomainParticipantFactory::get_instance();
     let participant = factory
@@ -74,6 +74,78 @@ fn test_volatile() {
         .unwrap();
 
     assert_eq!(samples[0].data().unwrap().value, 5);
+    assert_eq!(samples.len(), 1);
+}
+
+#[test]
+fn test_volatile_reliable() {
+    let domain_id = next_domain_id();
+    let factory = DomainParticipantFactory::get_instance();
+    let participant = factory
+        .create_participant(domain_id, DomainParticipantQos::default(), None, StatusMask::default())
+        .unwrap();
+
+    let writer_qos = DataWriterQos {
+        history: HistoryQosPolicy {
+            kind: HistoryQosPolicyKind::KeepLast(10),
+            ..Default::default()
+        },
+        reliability: ReliabilityQosPolicy {
+            kind: ReliabilityQosPolicyKind::Reliable,
+            max_blocking_time: Duration { sec: 0, nanosec: 100_000_000 },
+        },
+        ..Default::default()
+    };
+
+    let data_writer = create_datawriter(&participant, PublisherQos::default(), writer_qos);
+
+    // Write data from seq 0 to 4 before matching
+    for i in 0..=4 {
+        data_writer.write(&KeyedDataType::new(1, i), InstanceHandle::NIL).unwrap();
+    }
+
+    let reader_qos = DataReaderQos {
+        reliability: ReliabilityQosPolicy {
+            kind: ReliabilityQosPolicyKind::Reliable,
+            max_blocking_time: Duration { sec: 0, nanosec: 100_000_000 },
+        },
+        history: HistoryQosPolicy {
+            kind: HistoryQosPolicyKind::KeepLast(10),
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    let data_reader = create_datareader(&participant, SubscriberQos::default(), reader_qos);
+
+    wait_for_reader_status(
+        &data_reader,
+        StatusMask::SUBSCRIPTION_MATCHED,
+        Duration::from_seconds(1),
+    )
+    .unwrap();
+    wait_for_writer_status(
+        &data_writer,
+        StatusMask::PUBLICATION_MATCHED,
+        Duration::from_seconds(1),
+    )
+    .unwrap();
+
+    data_writer.write(&KeyedDataType::new(1, 5), InstanceHandle::NIL).unwrap();
+
+    wait_for_reader_status(&data_reader, StatusMask::DATA_AVAILABLE, Duration::from_seconds(1))
+        .unwrap();
+    let samples = data_reader
+        .take(
+            10,
+            &[SampleStateKind::ANY_SAMPLE_STATE],
+            &[ViewStateKind::ANY_VIEW_STATE],
+            &[InstanceStateKind::ANY_INSTANCE_STATE],
+        )
+        .unwrap();
+
+    assert_eq!(samples[0].data().unwrap().value, 5);
+    assert_eq!(samples.len(), 1);
 }
 
 #[test]
