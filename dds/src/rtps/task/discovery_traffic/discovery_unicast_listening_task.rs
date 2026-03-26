8313f696ae0c7ea9a92b9abd014bd22302fb5046
Intellctus-shinsungmin <conda.shin@int2.us>
2026-03-26 21:39:04 +0900

    FEAT: process spdp_message in discovery unicast for tcp mode

--- dds/src/rtps/task/discovery_traffic/discovery_unicast_listening_task.rs
+++ dds/src/rtps/task/discovery_traffic/discovery_unicast_listening_task.rs
@@ -1,14 +1,19 @@
+use crate::common::instance_handle::InstanceHandle;
 use crate::rtps::common::guid::GuidPrefix;
 use crate::rtps::common::rtps_error_code::{RtpsError, RtpsErrorCode, RtpsResult};
+use crate::rtps::common::types::DomainId;
 use crate::rtps::entities::entity::Entity;
 use crate::rtps::entities::participant::Participant;
+use crate::rtps::logic::message_processor::participant_message_processor::ParticipantMessageProcessor as _;
 use crate::rtps::logic::message_processor::unicast_message_processor::UnicastMessageProcessor as _;
 use crate::rtps::logic::sedp_logic::SedpLogic;
+use crate::rtps::logic::spdp_logic::SpdpLogic;
 use crate::rtps::messages::message_receiver::MessageReceiver;
 use crate::rtps::transport::socket::MAX_EVENTS;
 use crate::rtps::transport::udp::udp_listener::UdpListener;
+use crate::serialize::pl_cdr::InlineQosParameters;
 use crossbeam_channel::Receiver;
-use log::{debug, info, warn};
+use log::{debug, error, info, warn};
 use mio::{Events, Interest, Poll, Token};
 use std::net::SocketAddr;
 use std::sync::{Arc, Weak};
@@ -16,9 +21,11 @@ use std::time::Duration;
 
 pub(crate) struct DiscoveryUnicastListeningTask {
     guid_prefix: GuidPrefix,
+    domain_id: DomainId,
     discovery_unicast_listener: Option<UdpListener>,
     tcp_rx: Option<Receiver<(Vec<u8>, SocketAddr)>>,
     participant: Weak<Participant>,
+    spdp_logic: Arc<Option<SpdpLogic>>,
     sedp_logic: Arc<Option<SedpLogic>>,
 }
 
@@ -28,13 +35,16 @@ impl DiscoveryUnicastListeningTask {
         tcp_rx: Option<Receiver<(Vec<u8>, SocketAddr)>>,
         participant: Arc<Participant>,
     ) -> Self {
-        let (_, sedp_logic, _) = participant.get_logics();
+        let (spdp_logic, sedp_logic, _) = participant.get_logics();
         let guid_prefix = participant.guid().prefix();
+        let domain_id = participant.domain_id();
         Self {
             guid_prefix,
+            domain_id,
             discovery_unicast_listener,
             tcp_rx,
             participant: Arc::downgrade(&participant),
+            spdp_logic,
             sedp_logic,
         }
     }
@@ -154,6 +164,43 @@ impl DiscoveryUnicastListeningTask {
             return Ok(());
         }
 
+        // Check if this is an SPDP message (TCP mode sends SPDP via unicast)
+        if self.is_spdp_message(&message_receiver) {
+            let participant_proxy_data =
+                message_receiver.extract_participant_proxy_data(self.domain_id);
+            if let Some((participant_proxy_data, inline_qos_params)) = participant_proxy_data {
+                let spdp_logic = self.spdp_logic.as_ref().as_ref().ok_or_else(|| {
+                    RtpsError::new(RtpsErrorCode::DataNotSet, "SpdpLogic is not initialized")
+                })?;
+
+                let is_termination = inline_qos_params
+                    .as_ref()
+                    .and_then(|qos| qos.get_status_info())
+                    .is_some_and(|status| status.disposed() || status.unregistered());
+
+                if is_termination {
+                    let terminated_participant_guid = inline_qos_params
+                        .as_ref()
+                        .and_then(|qos| qos.get_key_hash())
+                        .unwrap_or_else(|| {
+                            InstanceHandle::from_guid(&participant_proxy_data.participant_guid())
+                        });
+
+                    if let Err(e) = spdp_logic.handle_participant_termination_message(
+                        &terminated_participant_guid.to_guid(),
+                    ) {
+                        error!("Failed to handle participant termination message: {:?}", e);
+                    }
+                } else if let Err(e) =
+                    spdp_logic.handle_discovered_participant_data(participant_proxy_data)
+                {
+                    error!("[DiscoveryUnicast] Failed to handle SPDP data: {:?}", e);
+                }
+            }
+            return Ok(());
+        }
+
+        // SEDP processing
         let participant = self.participant.upgrade().ok_or_else(|| {
             RtpsError::new(RtpsErrorCode::ArcUpgradeError, "Participant already dropped")
         })?;
@@ -162,7 +209,7 @@ impl DiscoveryUnicastListeningTask {
             .as_ref()
             .as_ref()
             .ok_or_else(|| {
-                RtpsError::new(RtpsErrorCode::DataNotSet, "SpdpLogic is not initialized")
+                RtpsError::new(RtpsErrorCode::DataNotSet, "SedpLogic is not initialized")
             })?
             .clone();
 
@@ -171,6 +218,21 @@ impl DiscoveryUnicastListeningTask {
             wlp_logic.handle_rtps_message(message_receiver)?;
         }
         Ok(())
+    }
+
+    /// Check if the RTPS message contains SPDP data (writer_id == SPDP_BUILTIN_PARTICIPANT_WRITER)
+    fn is_spdp_message(&self, message_receiver: &MessageReceiver) -> bool {
+        use crate::rtps::common::entity_id::EntityId;
+        use crate::rtps::messages::message_receiver::TypedSubmessage;
+
+        for submessage in message_receiver.parse_submessages() {
+            if let TypedSubmessage::Data(_, data) = submessage {
+                if data.writer_id == EntityId::SPDP_BUILTIN_PARTICIPANT_WRITER {
+                    return true;
+                }
+            }
+        }
+        false
     }
 }
 
@@ -189,8 +251,12 @@ mod tests {
         let domain_id: DomainId = 17;
         let mut socket = Socket::new(domain_id);
         socket.create_socket();
-        let participant =
-            Arc::new(Participant::new(domain_id, socket.participant_id(), socket.working_ips()));
+        let participant = Arc::new(Participant::new(
+            domain_id,
+            socket.participant_id(),
+            socket.working_ips(),
+            None,
+        ));
 
         let mut discovery_unicast_listening_task = DiscoveryUnicastListeningTask::new(
             socket.discovery_unicast_listener(),
