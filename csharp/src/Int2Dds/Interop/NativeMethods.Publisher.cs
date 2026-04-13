ca7bf7abba8bdb79c757f2806c8d69af183d5fed
Intellectus-Gahee <gh.park@int2.us>
2026-04-13 11:20:58 +0900

    FEAT : Add C# bindings for create_datareader/datawriter_with_profile FFI functions

--- csharp/src/Int2Dds/Interop/NativeMethods.Publisher.cs
+++ csharp/src/Int2Dds/Interop/NativeMethods.Publisher.cs
@@ -33,6 +33,14 @@ namespace Int2Dds.Interop
         internal static unsafe extern int int2dds_create_datawriter_with_listener(IntPtr publisher, IntPtr topic, IntPtr qos, NativeDataWriterListener* listener, uint mask, out IntPtr writer_out);
 
         [DllImport("int2dds_ffi", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int int2dds_create_datawriter_with_profile(IntPtr publisher, IntPtr topic,
+            [MarshalAs(UnmanagedType.LPUTF8Str)] string qos_path, out IntPtr writer_out);
+
+        [DllImport("int2dds_ffi", CallingConvention = CallingConvention.Cdecl)]
+        internal static unsafe extern int int2dds_create_datawriter_with_profile_and_listener(IntPtr publisher, IntPtr topic,
+            [MarshalAs(UnmanagedType.LPUTF8Str)] string qos_path, NativeDataWriterListener* listener, uint mask, out IntPtr writer_out);
+
+        [DllImport("int2dds_ffi", CallingConvention = CallingConvention.Cdecl)]
         internal static unsafe extern int int2dds_datawriter_set_listener(IntPtr writer, NativeDataWriterListener* listener, uint mask);
 
         [DllImport("int2dds_ffi", CallingConvention = CallingConvention.Cdecl)]
