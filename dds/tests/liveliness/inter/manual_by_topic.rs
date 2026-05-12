b8379416610ee0694ae96deee36a01e100d7656c
Intellectus-Garam <ran.kim@int2.us>
2026-05-12 10:48:17 +0900

    CHORE: Comment out logging

--- dds/tests/liveliness/inter/manual_by_topic.rs
+++ dds/tests/liveliness/inter/manual_by_topic.rs
@@ -142,6 +142,9 @@ fn assert_keeps_alive() {
 
 #[test]
 fn unmatch_via_delete_participant() {
+    // int2dds::common::env::set_log_type(int2dds::common::log::LogType::File);
+    // int2dds::common::env::set_file_log_level(int2dds::common::log::LogLevel::Debug);
+
     // Dropping the writer participant must unmatch via SEDP dispose.
     let s = Scenario::inter();
     let (wqos, rqos) = qos_pair();
