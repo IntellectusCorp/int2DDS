851cca9ae670bcefabe0cd2d95d0f9e16d263c73
Intellectus-Sub <sub.lee@int2.us>
2026-08-11 13:07:58 +0900

    FIX: Carry the reliability builtin endpoints are declared with

--- dds/src/rtps/logic/message_processor/participant_message_processor.rs
+++ dds/src/rtps/logic/message_processor/participant_message_processor.rs
@@ -442,7 +442,7 @@ pub(crate) trait ParticipantMessageProcessor: ParticipantAccessor {
                 false,
                 true,
                 // 8.5.4.1 According to the DDS specification, the reliability QoS for these built-in Entities is set to 'reliable.'
-                SubscriptionBuiltinTopicData::default(),
+                SubscriptionBuiltinTopicData::builtin_reliable(),
                 SequenceNumber::new(0, 0), // Built-in endpoints are not volatile
             );
             writer.matched_reader_add(reader_proxy);
