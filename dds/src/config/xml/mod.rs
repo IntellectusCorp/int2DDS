a9d9f29bf3b3a152ee0c14b78af3ba1018cb1f1e
Intellectus-Garam <ran.kim@int2.us>
2026-08-19 18:31:04 +0900

    CHORE: Group split-feature integration tests into per-feature folders

--- dds/src/config/xml/mod.rs
+++ dds/src/config/xml/mod.rs
@@ -460,7 +460,8 @@ mod tests {
 
     use super::*;
     use crate::dcps::topic::type_support::DdsType;
-    use crate::serialize::WString;
+    use crate::serialize::cdr::PrimitiveSerialize;
+    use crate::serialize::{DeserializerReader, WString};
     use crate::xtypes::{
         CompleteStructMember, CompleteStructType, ExtensibilityKind, HasTypeObject, MemberFlag,
         TryConstructKind, TypeFlag,
